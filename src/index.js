cb978630276bba560d4861f449f11a8882f57ab9
Davidlloyd8 <extendavidba4@gmail.com>
2022-09-15 16:40:34 +0100

    Make changes to the file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
